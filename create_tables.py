c7bdc6fa49714312fa8fbf4f209f8aa2899f9bc5
Abdulrahman <asg.1999@outlook.com>
2025-03-06 19:42:33 +0300

    Commit for 2nd submission

--- create_tables.py
+++ create_tables.py
@@ -2,31 +2,48 @@ import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
-
 def drop_tables(cur, conn):
+    """
+    Drops existing tables if they exist in Redshift.
+    
+    Parameters:
+        cur: Cursor object to execute PostgreSQL commands.
+        conn: Connection to the Redshift database.
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
-
 def create_tables(cur, conn):
+    """
+    Creates tables in Redshift as per the schema defined in sql_queries.py.
+    
+    Parameters:
+        cur: Cursor object to execute PostgreSQL commands.
+        conn: Connection to the Redshift database.
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
-
 def main():
+    """
+    - Establishes connection to Redshift.
+    - Calls functions to drop existing tables and create new tables.
+    - Closes the connection.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect(
+        "host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values())
+    )
     cur = conn.cursor()
 
     drop_tables(cur, conn)
     create_tables(cur, conn)
 
     conn.close()
-
 
 if __name__ == "__main__":
     main()
--- etl.py
+++ etl.py
@@ -2,31 +2,59 @@ import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
-
 def load_staging_tables(cur, conn):
+    """
+    Loads data from S3 into staging tables in Redshift using the COPY command.
+    
+    Parameters:
+        cur: Cursor object to execute PostgreSQL commands.
+        conn: Connection to the Redshift database.
+    """
+    print("Loading staging tables...")
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
-
+    
+    print("Staging tables loaded.")
 
 def insert_tables(cur, conn):
+    """
+    Inserts data from staging tables into fact and dimension tables.
+    
+    Parameters:
+        cur: Cursor object to execute PostgreSQL commands.
+        conn: Connection to the Redshift database.
+    """
+    
+    print("Inserting into final tables...")
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
+    
+    print("Final tables inserted.")
 
 
 def main():
+    """
+    - Establishes connection to Redshift.
+    - Calls functions to load staging tables and insert data into analytics tables.
+    - Closes the connection.
+    """
+    
+    print("Starting ETL process...")
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect(
+        "host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values())
+    )
     cur = conn.cursor()
-    
+
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
     conn.close()
-
+    print("ETL process completed successfully.")
 
 if __name__ == "__main__":
     main()
